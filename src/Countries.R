43aa903081a070be21355bf68253a29a1e8b9ed3
Chen <che161@csiro.au>
2019-09-20 13:43:46 +1000

    day4 save 2

--- src/Countries.R
+++ src/Countries.R
@@ -37,3 +37,59 @@ filter(gapminder, year >= 1997)
 filter(gapminder, continent == "Europe")
 filter(gapminder, lifeExp >= 80, gdpPercap >= 30000)
 filter(gapminder, lifeExp >= 80 | gdpPercap >= 30000)
+gapminder <- read_csv("data/gapminder.csv")
+filter(gapminder, country == "Australia")
+gapminder_Aus <- filter(gapminder, country == "Australia")
+glimpse(gapminder_Aus)
+select(gapminder_Aus,country,year,pop)
+select(filter(gapminder, country == "Australia"), country,year,pop)
+select(filter(gapminder, country == "Australia"), country,year,pop)
+filter(select(gapminder,country,year,pop), country == "Australia")
+
+#pipe
+gapminder %>% select(year,pop)
+gapminder %>%  filter(country == "Australia", year>= 1997)
+small <- gapminder %>%  filter(country == "Australia", year>= 1997)
+glimpse(small)
+small
+newgap <-  gapminder %>%  
+  filter(country == "Australia") %>%  
+  select(country,year,pop) %>%
+  rename(population = pop)
+newgap
+
+#mutate
+mutate(gapminder, gdp = gdpPercap*pop)
+mutate(gapminder, popInMillion = pop/1e6)
+mutate(gapminder, Log_pop = log(pop))
+str_sub("A long bit of text", start = 1, end =5)
+mutate(gapminder,coutry_abbr = str_sub(country, start = 1, end = 3))
+mutate(gapminder,coutry_namelength = str_length(country))
+mutate(gapminder,gdp = gdpPercap*pop,Log_pop = log(pop),coutry_namelength = str_length(country))
+mutate(
+  gapminder,
+  gdp = gdpPercap*pop,
+  Log_pop =  log(pop)
+)
+mutate(
+  gapminder,
+  lifeExp_days = lifeExp*365,
+  Gdp_bil =  gdpPercap*pop/1e9
+)
+
+#Summarising data
+summarise(gapminder, mean_life_exp = mean(lifeExp))
+summarise(gapminder, mean_life_exp = mean(lifeExp), mean_life_sd = sd(lifeExp), bigggest_gdp = max(gdpPercap))
+summarise(gapminder, mean_life_exp = mean(pop), median_life_sd = median(pop))
+summarise_if(gapminder, is.numeric, mean)
+
+#group_by
+group_by(gapminder, country)
+by_country <- group_by(gapminder, country)
+by_country
+summarise(by_country, mean_life_exp = mean(pop), median_life_sd = median(pop))
+by_continent <- group_by(gapminder, continent)
+by_continent 
+summarise(by_continent, mean_life_exp = mean(pop), median_life_sd = median(pop))
+gapminder %>% group_by(continent) %>% 
+  summarise(mean(pop), median_life_sd = median(pop))
